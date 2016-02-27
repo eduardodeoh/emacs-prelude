31c7261a258a2d48bc6cecd8a575bb7ba9ba877f
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2016-02-27 15:14:23 -0300

    Add my-prelude-modules

--- personal/setup-company.el
+++ personal/setup-company.el
@@ -1,6 +1,6 @@
 (setq company-show-numbers t)
 (setq company-transformers '(company-sort-by-occurrence))
-;; (define-key company-active-map [tab] 'company-complete-common-or-cycle)
+(define-key company-active-map [tab] 'company-complete-common-or-cycle)
 (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
